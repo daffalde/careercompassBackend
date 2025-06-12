0f1f639e5530a5a205682535e345626256b1d2dc
daffalde <daffalderayhan@gmail.com>
2025-06-12 21:56:42 +0700

    fixing all lowongan

--- src/route/lowonganController.js
+++ src/route/lowonganController.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {
+  getAllData,
   getData,
   getDataId,
   handleDelete,
@@ -10,6 +11,7 @@ import { authToken } from "../middleware/authToken.js";
 
 export const routerLowongan = express.Router();
 
+routerLowongan.get("/all-lowongan", getAllData);
 routerLowongan.get("/lowongan", getData);
 routerLowongan.get("/lowongan/:id", getDataId);
 routerLowongan.post("/lowongan", authToken, handleInsert);
